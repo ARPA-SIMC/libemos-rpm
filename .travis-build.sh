906ab343af3510e5cbd25d6966ee44a77333c359
Emanuele Di Giacomo <emanuele@digiacomo.cc>
2018-02-07 11:04:48 +0100

    travis

---
+++ .travis-build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -ex
+
+image=$1
+
+if [[ $image =~ ^centos: ]]
+then
+    pkgcmd="yum"
+    builddep="yum-builddep"
+    sed -i '/^tsflags=/d' /etc/yum.conf
+    yum install -y epel-release
+    yum install -y @buildsys-build
+    yum install -y yum-utils
+    yum install -y git
+    yum install -y rpmdevtools
+    yum install -y pv
+elif [[ $image =~ ^fedora: ]]
+then
+    pkgcmd="dnf"
+    builddep="dnf builddep"
+    sed -i '/^tsflags=/d' /etc/dnf/dnf.conf
+    dnf install -y @buildsys-build
+    dnf install -y 'dnf-command(builddep)'
+    dnf install -y git
+    dnf install -y rpmdevtools
+    dnf install -y pv
+fi
+
+$builddep -y libemos.spec
+
+if [[ $image =~ ^fedora: || $image =~ ^centos: ]]
+then
+    pkgname="$(rpmspec -q --qf="libemos-%{version}-%{release}\n" libemos.spec | head -n1)"
+    mkdir -p ~/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+    cp libemos.spec ~/rpmbuild/SPECS/
+    cp *.patch ~/rpmbuild/SOURCES/
+    spectool -g -R ~/rpmbuild/SPECS/libemos.spec
+    rpmbuild -ba ~/rpmbuild/SPECS/libemos.spec 2>&1 | pv -q -L 3k
+    find ~/rpmbuild/{RPMS,SRPMS}/ -name "${pkgname}*rpm" -exec cp -v {} . \;
+    # TODO upload ${pkgname}*.rpm to github release on deploy stage
+else
+    echo "Unsupported image"
+    exit 1
+fi
